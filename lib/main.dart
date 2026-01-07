b69c76789fe35da1f74d5bd01db8efbda990aa80
ismail <ismailcoder366@gmail.com>
2026-01-07 17:13:12 +0500

    font family package added for font styling, screenutils package added for responsiveness

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,56 @@
 import 'package:echotune/modules/landing/view/landing_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+    return ScreenUtilInit(
+      designSize: const Size(375, 822), // change based on your base design
+      minTextAdapt: true,
+      splitScreenMode: true,
 
-      home: LandingView()
+      builder: (context, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'EchoTune',
+
+          /// --------------------------
+          /// Named Routing with GetX
+          /// --------------------------
+          initialRoute: '/landing',
+
+          getPages: [
+            GetPage(
+              name: '/landing',
+              page: () => const LandingView(),
+            ),
+            // add more routes here
+            // GetPage(name: '/login', page: () => const LoginView()),
+          ],
+
+          /// --------------------------
+          /// Theme + Font Family
+          /// --------------------------
+            theme: ThemeData(
+              textTheme: GoogleFonts.poppinsTextTheme(
+                Theme.of(context).textTheme,
+              ),
+            ),
+          home: child,
+        );
+      },
+
+      child: const LandingView(),
     );
   }
 }
